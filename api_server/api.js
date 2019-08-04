9a983df74c4c56a2842d773fe061db7a90849ea2
davidmoradi <davidmoradi@protonmail.com>
2019-08-04 21:57:44 +0200

    blockchain and transaction endpoints

--- api_server/api.js
+++ api_server/api.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain/blockchain');
+
+const kisaCoin = new Blockchain();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello KisaCoin!')
 });
 
-app.get('/blockchain', (req, res) => {});
-app.post('/transaction', (req, res) => {});
+app.get('/blockchain', (req, res) => {
+  res.send(kisaCoin);
+});
+
+app.post('/transaction', (req, res) => {
+  const blockIndex = kisaCoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ info: `Tranaction will be mined in block number: ${blockIndex}`});
+});
+
 app.get('/mine', (req, res) => {});
 
 app.listen(port, () => {
