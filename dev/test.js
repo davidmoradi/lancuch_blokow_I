177dd417865fb75006ecac7cb2bf995acba39c5c
davidmoradi <davidmoradi@protonmail.com>
2019-08-04 14:29:13 +0200

    constructor and new block functionality

---
+++ dev/test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const kisacoin = new Blockchain;
+
+kisacoin.createNewBlock(1234, 'OIEKJDS8783Jh', 'TWGSJS736S7DHAF');
+kisacoin.createNewBlock(823, 'AWURHSAKJDA83', '834YRHASHJDJ9');
+kisacoin.createNewBlock(38, '73RJAKSFH3DJD0', '2364TRGAHJSHF');
+
+console.log(kisacoin);
