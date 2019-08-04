ea5056ce18480fbd5084143f2a350fdfec364d39
davidmoradi <davidmoradi@protonmail.com>
2019-08-04 15:12:47 +0200

    Allow creating new transactions and add them to pending

--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,14 @@ const Blockchain = require('./blockchain');
 const kisacoin = new Blockchain;
 
 kisacoin.createNewBlock(1234, 'OIEKJDS8783Jh', 'TWGSJS736S7DHAF');
+
+kisacoin.createNewTransaction(12, 'DF82UWRH8AS', '83UERUASDIASDU');
+kisacoin.createNewTransaction(4, 'DF82UWRH8AS', '83UERUASDIASDU');
 kisacoin.createNewBlock(823, 'AWURHSAKJDA83', '834YRHASHJDJ9');
-kisacoin.createNewBlock(38, '73RJAKSFH3DJD0', '2364TRGAHJSHF');
+
+kisacoin.createNewTransaction(432, 'DF82UWRH8AS', '83UERUASDIASDU');
+kisacoin.createNewTransaction(7, 'DF82UWRH8AS', '83UERUASDIASDU');
+kisacoin.createNewTransaction(91283, 'DF82UWRH8AS', '83UERUASDIASDU');
+kisacoin.createNewBlock(3831, '73RJAKSFH3DJD0', '2364TRGAHJSHF');
 
 console.log(kisacoin);
